6f2f2dca68f07fcaa752e1ff19b9f0ac07c1007b
MarkLombardiNelson <marklombardinelson@gmail.com>
2016-12-12 10:14:46 -0500

    Hour 1 In-Class Explorer Mode

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,55 @@
+class StoriesController < ApplicationController
+  # GET /stories
+  def index
+    @stories = Story.all
+  end
+
+  # GET /stories/1
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  # GET /stories/new
+  def new
+    @story = Story.new
+  end
+
+  # GET /stories/1/edit
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  # POST /stories
+  def create
+    @story = Story.new(story_params)
+
+    if @story.save
+      redirect_to @story, notice: 'Story was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /stories/1
+  def update
+    @story = Story.find(params[:id])
+    if @story.update(story_params)
+      redirect_to @story, notice: 'Story was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /stories/1
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+    redirect_to stories_url, notice: 'Story was successfully destroyed.'
+  end
+
+  private
+    # Only allow a trusted parameter "white list" through.
+    def story_params
+      params.require(:story).permit(:title, :link, :email)
+    end
+end
