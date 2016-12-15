c07dc9c1b8afa48d535f21f44f248d53fbbf9802
Jonthan Hutson <jhut110766@gmail.com>
2016-12-15 12:07:05 -0500

    login with github

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ gem 'turbolinks', '~> 5'
 # Authentication and Authorization
 gem 'devise'
 
+gem 'omniauth'
+
+gem 'omniauth-github'
+
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215152628) do
+ActiveRecord::Schema.define(version: 20161215170319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20161215152628) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "user_name"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
