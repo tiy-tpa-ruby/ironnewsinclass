9711642190818a37029493d5cb0451954d1fbca4
MarkLombardiNelson <marklombardinelson@gmail.com>
2016-12-20 11:13:17 -0500

    Hiding and Showing Truncated Links

---
+++ app/assets/javascripts/stories_index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $('.full-link').hide()
+  $('.truncated-link').click(function(event) {
+    $(this).hide().siblings('.full-link').show()
+  })
+
+  $('.full-link').click(function(event) {
+    $(this).hide().siblings('.truncated-link').show()
+  })
+})
+
+// Alternative
+
+// function whatToDoWhenClicked(event) {
+//   console.log('Hello World')
+// }
+//
+// function whatToDoOnLoad() {
+//   $('.truncated-link').click(whatToDoWhenClicked)
+// }
+//
+// $(document).ready(whatToDoOnLoad)
+
+// Ruby Version
+// jquery(document).ready do
+//   jquery(".truncated-link").click do |event|
+//     console.log('Hello World')
+//   end
+// end
